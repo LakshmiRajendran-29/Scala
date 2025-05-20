4540f91ad0f572471eea365d28a1e01320c554d4
Lakshmi Rajendran <lakshmitanhars@gmail.com>
2025-05-20 17:34:07 +0100

    Create sumofArray.scala

---
+++ src/test/scala/FirstPackage/sumofArray.scala
@@ -0,0 +1,16 @@
+object May20 {
+
+  def main(args: Array[String]): Unit = {
+
+  // sum of Array Elements
+    var arr = Array(10,20,30,40,50,60)
+    var sum =0
+    for(i<- 0 until arr.length){
+      sum +=arr(i)
+    }
+
+    println(sum)
+  }
+
+
+}
